85ca1dae89cf53fae9cb5c71eedb643c4ebf5cc5
fork_me_maybe <forkmemaybe@gmail.com>
2024-11-06 10:59:28 +0530

    add dunst notification to save_dotfile.sh

--- save_dotfiles.sh
+++ save_dotfiles.sh
@@ -13,8 +13,15 @@ selected_role=$(echo "$roles" | rofi -dmenu -p "Select a role to save:")
 if [[ -n "$selected_role" ]]; then
     # Run the ansible-playbook command for the selected role
     cd "$HOME/repos/dotfiles" || exit
-    ./dotfiles save-"$selected_role"
+    if ./dotfiles save-"$selected_role"; then
+        # If successful, notify of success
+        notify-send -u normal -i ~/.config/dunst/icons/success.png "Dotfiles saved" "Role '$selected_role' saved successfully."
+    else
+        # If unsuccessful, notify of failure
+        notify-send -u critical -i ~/.config/dunst/icons/error.png "Error" "Failed to save role '$selected_role'."
+    fi
 else
     echo "No role selected."
+    notify-send -u low -i ~/.config/dunst/icons/notification.png "No role selected."
 fi
 
