552446c8e596c3186262d2473df8a76cee97c65f
fork_me_maybe <forkmemaybe@gmail.com>
2025-05-07 11:51:50 +0530

    updte nvim

--- roles/neovim/files/nvim/lua/odd/plugins/auto-save.lua
+++ roles/neovim/files/nvim/lua/odd/plugins/auto-save.lua
@@ -6,36 +6,88 @@
 -- Filename: ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/auto-save.lua
 -- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/auto-save.lua
 
+-- return {
+--     {
+--         "okuuva/auto-save.nvim",
+--         cmd = "ASToggle", -- optional for lazy loading on command
+--         event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+--         opts = {
+--             --
+--             -- All of these are just the defaults
+--             --
+--             enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+--             trigger_events = { -- See :h events
+--                 immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
+--                 defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+--                 cancel_deferred_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
+--             },
+--             -- function that takes the buffer handle and determines whether to save the current buffer or not
+--             -- return true: if buffer is ok to be saved
+--             -- return false: if it's not ok to be saved
+--             -- if set to `nil` then no specific condition is applied
+--             condition = nil,
+--             write_all_buffers = false, -- write all buffers when the current one meets `condition`
+--             -- Do not execute autocmds when saving
+--             -- This is what fixed the issues with undo/redo that I had
+--             -- https://github.com/okuuva/auto-save.nvim/issues/55
+--             noautocmd = false,
+--             lockmarks = false, -- lock marks when saving, see `:h lockmarks` for more details
+--             -- delay after which a pending save is executed (default 1000)
+--             debounce_delay = 1000, -- This could be adjusted slightly to allow time for formatting
+--             -- log debug messages to 'auto-save.log' file in neovim cache directory, set to `true` to enable
+--             debug = false,
+--         },
+--         config = function()
+--             -- Hook into the auto-save plugin to delay format after a paste
+--             vim.api.nvim_create_autocmd("TextChanged", {
+--                 group = vim.api.nvim_create_augroup("AutoSaveFormatting", { clear = true }),
+--                 callback = function()
+--                     -- Check if a format action is available, if so format after the delay
+--                     vim.defer_fn(function()
+--                         if vim.lsp.buf.format then
+--                             vim.lsp.buf.format({ async = false })
+--                         end
+--                     end, 50) -- 50ms delay to make sure paste is done
+--                 end,
+--             })
+--         end,
+--     },
+-- }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 return {
     {
         "okuuva/auto-save.nvim",
-        cmd = "ASToggle", -- optional for lazy loading on command
-        event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+        event = "VeryLazy", -- more reliable
         opts = {
-            --
-            -- All of these are just the defaults
-            --
-           enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
-           trigger_events = { -- See :h events
-                immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
-                defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
-                cancel_deferred_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
+            enabled = true,
+            trigger_events = {
+                immediate_save = { "BufLeave", "FocusLost" },
+                defer_save = { "InsertLeave", "TextChanged" },
+                cancel_deferred_save = { "InsertEnter" },
             },
-            -- function that takes the buffer handle and determines whether to save the current buffer or not
-            -- return true: if buffer is ok to be saved
-            -- return false: if it's not ok to be saved
-            -- if set to `nil` then no specific condition is applied
-            condition = nil,
-            write_all_buffers = false, -- write all buffers when the current one meets `condition`
-            -- Do not execute autocmds when saving
-            -- This is what fixed the issues with undo/redo that I had
-            -- https://github.com/okuuva/auto-save.nvim/issues/55
-            noautocmd = false,
-            lockmarks = false, -- lock marks when saving, see `:h lockmarks` for more details
-            -- delay after which a pending save is executed (default 1000)
             debounce_delay = 1000,
-            -- log debug messages to 'auto-save.log' file in neovim cache directory, set to `true` to enable
-            debug = false,
+            condition = function(buf)
+                -- optional: only save normal files, not help or terminal
+                local ft = vim.bo[buf].filetype
+                return vim.fn.getbufvar(buf, "&modifiable") == 1 and ft ~= "help" and ft ~= "terminal"
+            end,
+            noautocmd = false,
         },
     },
 }
--- roles/neovim/files/nvim/lua/odd/plugins/lsp/lspconfig.lua
+++ roles/neovim/files/nvim/lua/odd/plugins/lsp/lspconfig.lua
@@ -1,130 +1,256 @@
+-- return {
+--     "neovim/nvim-lspconfig",
+--     event = { "BufReadPre", "BufNewFile" },
+--     dependencies = {
+--         "hrsh7th/cmp-nvim-lsp",
+--         { "antosha417/nvim-lsp-file-operations", config = true },
+--         { "folke/neodev.nvim",                   opts = {} },
+--         { "nvimtools/none-ls.nvim" }, -- changed from null-ls
+--     },
+--     config = function()
+--         local lspconfig = require("lspconfig")
+--         local mason_lspconfig = require("mason-lspconfig")
+--         local cmp_nvim_lsp = require("cmp_nvim_lsp")
+--         local null_ls = require("null-ls") -- changed from require("null-ls")
+--
+--         vim.diagnostic.config({
+--             signs = {
+--                 text = {
+--                     [vim.diagnostic.severity.ERROR] = " ",
+--                     [vim.diagnostic.severity.WARN]  = " ",
+--                     [vim.diagnostic.severity.HINT]  = "󰠠 ",
+--                     [vim.diagnostic.severity.INFO]  = " ",
+--                 },
+--             },
+--         })
+--
+--         local keymap = vim.keymap
+--         vim.api.nvim_create_autocmd("LspAttach", {
+--             group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+--             callback = function(ev)
+--                 local opts = { buffer = ev.buf, silent = true }
+--
+--                 keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+--                 keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+--                 keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+--                 keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+--                 keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+--                 keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+--                 keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+--                 keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+--                 keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+--                 keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+--                 keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+--                 keymap.set("n", "K", vim.lsp.buf.hover, opts)
+--                 keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+--             end,
+--         })
+--
+--         local capabilities = cmp_nvim_lsp.default_capabilities()
+--
+--         mason_lspconfig.setup_handlers({
+--             function(server_name)
+--                 lspconfig[server_name].setup({
+--                     capabilities = capabilities,
+--                 })
+--             end,
+--             ["emmet_ls"] = function()
+--                 lspconfig["emmet_ls"].setup({
+--                     capabilities = capabilities,
+--                     filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+--                 })
+--             end,
+--             ["lua_ls"] = function()
+--                 lspconfig["lua_ls"].setup({
+--                     capabilities = capabilities,
+--                     settings = {
+--                         Lua = {
+--                             diagnostics = { globals = { "vim" } },
+--                             completion = { callSnippet = "Replace" },
+--                         },
+--                     },
+--                 })
+--             end,
+--         })
+--
+--         local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+--
+--         vim.api.nvim_create_autocmd("BufWritePre", {
+--             group = augroup,
+--             pattern = "*.py",
+--             callback = function()
+--                 vim.lsp.buf.format({
+--                     async = false,
+--                     filter = function(client)
+--                         return client.name == "null-ls"
+--                     end,
+--                 })
+--             end,
+--         })
+--
+--         null_ls.setup({
+--             sources = {
+--                 null_ls.builtins.formatting.black,
+--                 null_ls.builtins.diagnostics.sqruff,
+--             },
+--             on_attach = function(client, bufnr)
+--                 if client.supports_method("textDocument/formatting") then
+--                     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--                     vim.api.nvim_create_autocmd("BufWritePre", {
+--                         group = augroup,
+--                         buffer = bufnr,
+--                         callback = function()
+--                             vim.lsp.buf.format({ async = false })
+--                         end,
+--                     })
+--                 end
+--             end,
+--         })
+--     end,
+-- }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 return {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        { "antosha417/nvim-lsp-file-operations", config = true },
-        { "folke/neodev.nvim", opts = {} },
-        { "nvimtools/none-ls.nvim" },
-    },
-    config = function()
-        local lspconfig = require("lspconfig")
-        local mason_lspconfig = require("mason-lspconfig")
-        local cmp_nvim_lsp = require("cmp_nvim_lsp")
-        local null_ls = require("null-ls")  -- changed from require("null-ls")
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    { "antosha417/nvim-lsp-file-operations", config = true },
+    { "folke/neodev.nvim", opts = {} },
+    { "nvimtools/none-ls.nvim" }, -- formerly null-ls
+  },
+  config = function()
+    local lspconfig = require("lspconfig")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local null_ls = require("null-ls") -- formerly null-ls
 
-        local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-        end
+    vim.diagnostic.config({
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = " ",
+          [vim.diagnostic.severity.WARN]  = " ",
+          [vim.diagnostic.severity.HINT]  = "󰠠 ",
+          [vim.diagnostic.severity.INFO]  = " ",
+        },
+      },
+    })
 
-        local keymap = vim.keymap
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-            callback = function(ev)
-                local opts = { buffer = ev.buf, silent = true }
+    local keymap = vim.keymap
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(ev)
+        local opts = { buffer = ev.buf, silent = true }
 
-                keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
-                keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-                keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-                keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
-                keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
-                keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-                keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-                keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
-                keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-                keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-                keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-                keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
-            end,
-        })
+        keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+        keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+        keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+        keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+        keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+        keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+        keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+        keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+        keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+      end,
+    })
 
-        local capabilities = cmp_nvim_lsp.default_capabilities()
+    local capabilities = cmp_nvim_lsp.default_capabilities()
 
-        mason_lspconfig.setup_handlers({
-            function(server_name)
-                lspconfig[server_name].setup({
-                    capabilities = capabilities,
-                })
-            end,
-            ["emmet_ls"] = function()
-                lspconfig["emmet_ls"].setup({
-                    capabilities = capabilities,
-                    filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-                })
-            end,
-            ["lua_ls"] = function()
-                lspconfig["lua_ls"].setup({
-                    capabilities = capabilities,
-                    settings = {
-                        Lua = {
-                            diagnostics = { globals = { "vim" } },
-                            completion = { callSnippet = "Replace" },
-                        },
-                    },
-                })
-            end,
-        })
-
-        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            pattern = "*.py",
-            callback = function()
-                vim.lsp.buf.format({
-                    async = false,
-                    filter = function(client)
-                        return client.name == "null-ls"
-                    end,
-                })
-            end,
-        })
-
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.black.with({
-                    extra_args = { "--fast" },
-                }),
-                null_ls.builtins.diagnostics.sqruff,
-                null_ls.builtins.diagnostics.mypy.with({
-                    extra_args = { "--strict" },
-                }),
+    local servers = {
+      lua_ls = {
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+            completion = { callSnippet = "Replace" },
+          },
+        },
+      },
+      emmet_ls = {
+        filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+      },
+      pyright = {
+        settings = {
+          python = {
+            analysis = {
+              typeCheckingMode = "basic",
+              useLibraryCodeForTypes = true,
+              diagnosticMode = "workspace",
+              stubPath = "typings",
             },
-            on_attach = function(client, bufnr)
-                if client.supports_method("textDocument/formatting") then
-                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        group = augroup,
-                        buffer = bufnr,
-                        callback = function()
-                            vim.lsp.buf.format({ async = false })
-                        end,
-                    })
-                end
-            end,
-        })
+            pythonPath = os.getenv("VIRTUAL_ENV") and os.getenv("VIRTUAL_ENV") .. "/bin/python" or "python3",
+          },
+        },
+      },
+      clangd = {},
+      marksman = {},
+      bashls = {},
+      cssls = {},
+    }
 
-        -- null_ls.setup({
-        --     sources = {
-        --         null_ls.builtins.formatting.black,
-        --         null_ls.builtins.diagnostics.sqruff,
-        --     },
-        --     on_attach = function(client, bufnr)
-        --         if client.supports_method("textDocument/formatting") then
-        --             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        --             vim.api.nvim_create_autocmd("BufWritePre", {
-        --                 group = augroup,
-        --                 buffer = bufnr,
-        --                 callback = function()
-        --                     vim.lsp.buf.format({ async = false })
-        --                 end,
-        --             })
-        --         end
-        --     end,
-        -- })
-    end,
+    for server_name, config in pairs(servers) do
+      config.capabilities = capabilities
+      lspconfig[server_name].setup(config)
+    end
+
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      pattern = "*.py",
+      callback = function()
+        vim.lsp.buf.format({
+          async = false,
+          filter = function(client)
+            return client.name == "null-ls"
+          end,
+        })
+      end,
+    })
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.diagnostics.sqruff,
+      },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
+    })
+  end,
 }
 
